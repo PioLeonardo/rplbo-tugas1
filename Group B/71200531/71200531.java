1646b413ee21c2e78250606971b9d764e932971f
PioLeonardo <pio.leonardo@ti.ukdw.ac.id>
2022-02-10 23:46:01 +0700

    71200531 program java

---
+++ Group B/71200531/71200531.java
@@ -0,0 +1,10 @@
+public class Tugas71190461 {
+    public static void main(String[] args) {
+        int n = 531;
+        int tambah = 0;
+        for (int i = 0; i <= n; i++) {
+            sum = sum + i;
+        }
+    System.out.println(sum);
+    }
+}
